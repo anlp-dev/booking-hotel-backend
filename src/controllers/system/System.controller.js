b5e1fab292cb1632743351b388286d4da6e371ea
anlp-dev <anlp.sep19@gmail.com>
2025-03-21 11:11:23 +0700

    update code

--- src/controllers/system/System.controller.js
+++ src/controllers/system/System.controller.js
@@ -1,22 +1,59 @@
 const { resExport } = require("../../enums/resExport");
 const systemService = require("../../services/system/System.service")
+const { MESSAGE } = require("../../messages/message");
 
 class SystemController {
   async getRouter(req, res){
     try{
       const resData = await systemService.getAllRoute();
-      resExport(200, "Lấy danh sách route thành công", resData, res);
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
     }catch(e){
-      resExport(500, e.message, null, res);
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
     }
   }
 
   async createNewRoute(req, res){
     try{
       const resData = await systemService.createRoute(req.body);
-      resExport(200, "Tạo route thành công", resData, res);
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
     }catch(e){
-      resExport(500, e.message, null, res);
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
+    }
+  }
+
+  async updateRoute(req, res){
+    try{
+      const resData = await systemService.updateRoute(req.body);
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
+    }catch(e){
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
+    }
+  }
+
+  async deleteRoute(req, res){
+    try{
+      const resData = await systemService.deleteRoute(req.params.id);
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
+    }catch(e){
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
+    }
+  }
+
+  async getLogRequest(req, res){
+    try{
+      const resData = await systemService.getLogRequest(req.query);
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
+    }catch(e){
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
+    }
+  }
+
+  async getDashboard(req, res){
+    try{
+      const resData = await systemService.getDashBoard();
+      resExport(MESSAGE.SUCCESS.status, MESSAGE.SUCCESS.message, resData, res)
+    }catch(e){
+      resExport(MESSAGE.ERROR.status, e.message, null, res);
     }
   }
 }
